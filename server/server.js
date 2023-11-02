8938fd652295a80d3a3e8b1557a3d2d3e2815d1e
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-11-02 16:53:57 -0600

    did some changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
