9ace35ec6ba9813fc0d7ce0766e4f41061d66ac8
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-11-03 12:52:01 -0600

    deploying again 8

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
 
 
 // const password = 'Stanop09#';
 // const encodedPassword = encodeURIComponent(password);
-// require('dotenv').config();
-// const { connect, connection } = require('mongoose');
-// const connectionString = process.env.MONGODB_URI;
-// connect(connectionString);
-// module.exports = connection;
+require('dotenv').config();
+const { connect, connection } = require('mongoose');
+const connectionString = process.env.MONGODB_URI;
+connect(connectionString);
+module.exports = connection;
