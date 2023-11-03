12fba5109a3fadba56f1ec65f6f30f33e56f9535
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-11-03 11:07:02 -0600

    deploying again 6

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const encodedPassword = encodeURIComponent(password);
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://stanlou09:${encodedPassword}@bootcamp.mq1yue5.mongodb.net/?retryWrites=true&w=majority/googlebooks');
 
 module.exports = mongoose.connection;
 
